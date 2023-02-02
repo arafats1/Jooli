91c907bcc2f87b1f9e418b407c21cca193d586d1
Arafat <arafats144@gmail.com>
2023-02-02 20:27:23 +0300

    on change

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,13 @@
 import styles from './styles.module.css';
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h1>{`Jooli?`}</h1>
-        <input className={styles.input} placeholder='Username...' />
+        <input className={styles.input} placeholder='Username...' onChange={(e) => setUsername(e.target.value)} />
 
-        <select className={styles.input}>
+        <select className={styles.input} onChange={(e) => setRoom(e.target.value)}>
           <option>-- Select Room --</option>
           <option value='javascript'>Code</option>
           <option value='node'>Football</option>
