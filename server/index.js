5d1f02b8863fbff391e2e5e61c28d401fc7ba53a
Arafat <arafats144@gmail.com>
2023-02-22 07:15:34 +0300

    add a socket client

--- server/index.js
+++ server/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 app.use(cors()); // Add cors middleware
 
 const server = http.createServer(app);
+
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+  });
 
 app.get('/', (req, res) => {
     res.send('Hello world');
