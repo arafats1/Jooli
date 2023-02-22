386b4d2dc2ebc54ce7f935ae9cc27451117a1cca
Arafat <arafats144@gmail.com>
2023-02-22 07:28:27 +0300

    add a socket function

--- server/index.js
+++ server/index.js
@@ -11,20 +11,24 @@ const server = http.createServer(app);
 // Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
 const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'POST'],
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
     },
-  });
+});
 
-  // Listen for when the client connects via socket.io-client
+// Listen for when the client connects via socket.io-client
 io.on('connection', (socket) => {
     console.log(`User connected ${socket.id}`);
-  
+});
 
-  });
+// Add a user to a room
+socket.on('join_room', (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world');
-  });
+});
 
 server.listen(4000, () => 'Server is running on port 4000');
