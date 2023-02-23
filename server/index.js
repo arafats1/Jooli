7d770bd13e7c4a6f5280f8712d7a3b2fd9a68e70
Arafat <arafats144@gmail.com>
2023-02-23 15:41:18 +0300

    add receive message

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 
-app.use(cors()); // Add cors middleware
+app.use(cors()); 
 
 const server = http.createServer(app);
 
@@ -16,6 +16,8 @@ const io = new Server(server, {
     },
 });
 
+const CHAT_BOT = 'ChatBot';
+
 // Listen for when the client connects via socket.io-client
 io.on('connection', (socket) => {
     console.log(`User connected ${socket.id}`);
@@ -25,6 +27,13 @@ io.on('connection', (socket) => {
 socket.on('join_room', (data) => {
     const { username, room } = data; // Data sent from client when join_room event emitted
     socket.join(room); // Join the user to a socket room
+    let __createdtime__ = Date.now(); // Current timestamp
+    // Send message to all users currently in the room, apart from the user that just joined
+    socket.to(room).emit('receive_message', {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
 });
 
 app.get('/', (req, res) => {
