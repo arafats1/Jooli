5a4af64d628af7cd63842ea0b5202cfdd2068c86
Arafat <arafats144@gmail.com>
2023-03-02 21:21:22 +0300

    add dotenv

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 import MessagesReceived from './messages';
 import SendMessage from './send-message';
 
-const Chat = ({ socket }) => {
+const Chat = ({username, room, socket}) => {
   return (
     <div className={styles.chatContainer}>
       <div>
