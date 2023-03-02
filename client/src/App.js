eef65c09ce9220beacb5c8d584650559d54d0471
Arafat <arafats144@gmail.com>
2023-03-02 14:59:36 +0300

    add message received

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
+// client/src/App.js
 
 import './App.css';
-import { useState } from 'react'; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import io from 'socket.io-client'; 
+import { useState } from 'react';
 import Home from './pages/home';
+import Chat from './pages/chat';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:4000'); 
+const socket = io.connect('http://localhost:4000');
 
 function App() {
-  const [username, setUsername] = useState(''); 
-  const [room, setRoom] = useState(''); 
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
 
   return (
     <Router>
@@ -19,11 +21,11 @@ function App() {
             path='/'
             element={
               <Home
-                username={username} 
-                setUsername={setUsername} 
-                room={room} 
-                setRoom={setRoom} 
-                socket={socket} 
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
               />
             }
           />
