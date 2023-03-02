fd31271d82737976c478edde4d61ce5bb24be4c2
Arafat <arafats144@gmail.com>
2023-03-02 22:54:45 +0300

    leaving room

--- client/src/pages/chat/messages.js
+++ client/src/pages/chat/messages.js
@@ -1,8 +1,10 @@
 import styles from './styles.module.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Messages = ({ socket }) => {
   const [messagesRecieved, setMessagesReceived] = useState([]);
+
+  const messagesColumnRef = useRef(null);
 
   // Runs whenever a socket event is recieved from the server
   useEffect(() => {
@@ -21,6 +23,30 @@ const Messages = ({ socket }) => {
 	// Remove event listener on component unmount
     return () => socket.off('receive_message');
   }, [socket]);
+
+  useEffect(() => {
+    // Last 100 messages sent in the chat room (fetched from the db in backend)
+    socket.on('last_100_messages', (last100Messages) => {
+      console.log('Last 100 messages:', JSON.parse(last100Messages));
+      last100Messages = JSON.parse(last100Messages);
+      // Sort these messages by __createdtime__
+      last100Messages = sortMessagesByDate(last100Messages);
+      setMessagesReceived((state) => [...last100Messages, ...state]);
+    });
+    return () => socket.off('last_100_messages');
+  }, [socket]);
+
+  // Scroll to the most recent message
+  useEffect(() => {
+    messagesColumnRef.current.scrollTop =
+      messagesColumnRef.current.scrollHeight;
+  }, [messagesRecieved]);
+
+  function sortMessagesByDate(messages) {
+    return messages.sort(
+      (a, b) => parseInt(a.__createdtime__) - parseInt(b.__createdtime__)
+    );
+  }
 
   // dd/mm/yyyy, hh:mm:ss
   function formatDateFromTimestamp(timestamp) {
