d90749ec06c5e1e2fb770b3746ff6a7be4399204
Alaphat Magezi <87616467+arafats1@users.noreply.github.com>
2023-04-15 13:43:05 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methodss
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
 const io = new Server(server, {
     cors: {
         origin: 'http://localhost:3000',
